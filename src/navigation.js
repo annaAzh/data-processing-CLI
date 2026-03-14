32915b5bc14d264b4ab16d4855c67b0cc794a722
annaAzh <forarre@yandex.ru>
2026-03-14 09:40:17 +0300

    feat: implement cd, up, ls commands

--- src/navigation.js
+++ src/navigation.js
@@ -1,1 +1,35 @@
-// navigation commands (up, cd, ls)
+import { resolve } from 'path'
+import { getHomeDir } from './utils/pathResolver.js'
+import { readdir } from 'fs/promises'
+import { access } from 'node:fs/promises'
+
+export const upCommand = currentPath => {
+  const pathToUp = resolve(currentPath, '..')
+
+  if (currentPath === getHomeDir()) {
+    console.log(`You are already in ${getHomeDir()}`)
+    return currentPath
+  }
+  return pathToUp
+}
+
+export const cdCommand = async (currentPath, args) => {
+  const cdPath = resolve(currentPath, args[0])
+  await access(cdPath)
+  return cdPath
+}
+
+export const lsCommand = async currentPath => {
+  const info = await readdir(currentPath, { withFileTypes: true })
+
+  const sortedInfo = info
+    .sort((a, b) => {
+      if (a.isDirectory() !== b.isDirectory()) {
+        return a.isDirectory() ? -1 : 1
+      }
+      return a.name.localeCompare(b.name)
+    })
+    .map(item => ({ name: item.name, type: item.isDirectory() ? 'folder' : 'file' }))
+
+  console.table(sortedInfo)
+}
