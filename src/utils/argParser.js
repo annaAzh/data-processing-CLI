33c7b5fb7e4b27b439b564c3529f1c407b52a48c
annaAzh <forarre@yandex.ru>
2026-03-14 09:39:48 +0300

    feat: add parse args

--- src/utils/argParser.js
+++ src/utils/argParser.js
@@ -1,1 +1,57 @@
-// parse command line arguments
+import { getHomeDir } from './pathResolver.js'
+import { useStore } from '../state/currentDirectory.js'
+import { resolve, dirname } from 'path'
+
+export const argsParser = line => {
+  const [command, ...rest] = line
+    .trim()
+    .match(/'([^']*)'|"([^"]*)"|([^\s]+)/g)
+    .map(arg => arg.replace(/^['"]|['"]$/g, ''))
+
+  const args = rest.length > 0 ? [parsePath(rest[0]), ...rest.slice(1)] : []
+  return { command, args }
+}
+
+export const parsePath = line => {
+  const { getCurrentWorkingDir } = useStore()
+
+  let path = line.trim()
+
+  if (path.startsWith('--')) {
+    return path
+  }
+
+  if (path === '.' || path === './') {
+    return getCurrentWorkingDir()
+  }
+
+  if (path === '..' || path === '../') {
+    return dirname(getCurrentWorkingDir())
+  }
+
+  if (
+    (path.startsWith('"') && path.endsWith('"')) ||
+    (path.startsWith("'") && path.endsWith("'"))
+  ) {
+    path = path.slice(1, -1)
+  }
+
+  if (path.startsWith('~')) {
+    return getHomeDir()
+  }
+
+  if (path.startsWith('/') || path.startsWith('\\')) {
+    return path
+  }
+
+  if (
+    path.startsWith('./') ||
+    path.startsWith('../') ||
+    path.startsWith('-') ||
+    path.startsWith('.')
+  ) {
+    return resolve(getCurrentWorkingDir(), path)
+  }
+
+  return resolve(getCurrentWorkingDir(), path)
+}
