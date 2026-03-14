ab99a450ab6377f09b87ba022419d7ee91ef5b05
annaAzh <forarre@yandex.ru>
2026-03-14 09:40:46 +0300

    feat: add repl controller

--- src/repl.js
+++ src/repl.js
@@ -1,1 +1,35 @@
-// REPL handler, command parsing and dispatching
+import { upCommand, cdCommand, lsCommand } from './navigation.js'
+import { useStore } from './state/currentDirectory.js'
+import { printCurrentDirectory } from './utils/pathResolver.js'
+
+export const repl = async (command, args) => {
+  const { getCurrentWorkingDir, setCurrentWorkingDirectory } = useStore()
+  const currentPath = getCurrentWorkingDir()
+
+  try {
+    switch (command) {
+      case 'up': {
+        const targetPath = upCommand(currentPath)
+        printCurrentDirectory(targetPath)
+        setCurrentWorkingDirectory(targetPath)
+        break
+      }
+      case 'cd': {
+        const targetPath = await cdCommand(currentPath, args)
+        printCurrentDirectory(targetPath)
+        setCurrentWorkingDirectory(targetPath)
+        break
+      }
+      case 'ls': {
+        await lsCommand(currentPath)
+        printCurrentDirectory(currentPath)
+        break
+      }
+      default:
+        console.log('Invalid input')
+    }
+  } catch (error) {
+    console.log(error.message, '!!!!!!!')
+    console.log('Operation failed')
+  }
+}
